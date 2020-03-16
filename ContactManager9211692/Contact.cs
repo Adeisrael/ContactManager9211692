d5b6d094ae3fa9610bd5731ca0cfe0db11c49211
adeisrad <adeisrad@ML0248-15>
2020-03-16 09:59:37 +0000

    Creation of Classes

---
+++ ContactManager9211692/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager9211692
+{
+    class Contact
+    {
+        private int ID { get; set; }
+        private string contactFname { get; set; }
+        private string contactLname { get; set; }
+        private string contactTel { get; set; }
+        private string contactfirstLine { get; set;}
+        private string contactsecondLine { get; set; }
+        private string contactCity { get; set; }
+        private string contactPostcode { get;set; }
+
+        
+    }
+
+    
+}
---
+++ ContactManager9211692/PersonalContact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManager9211692
+{
+    class PersonalContact: Contact
+    {
+        private string contactHomeTel { get; set;}
+    }
+}
